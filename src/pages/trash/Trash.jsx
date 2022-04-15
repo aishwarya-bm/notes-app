2ed88fdf5acbc1df4fad71ab0e1e8e93cbfb7a79
aishwarya-bm <biradaraishwarya@gmail.com>
2022-04-16 00:41:37 +0530

    minor style changes

--- src/pages/trash/Trash.jsx
+++ src/pages/trash/Trash.jsx
@@ -2,7 +2,32 @@ import "./trash.css";
 import { Header, NoteCard, Sidenav } from "components";
 import { useState } from "react";
 export function Trash() {
-  const notes = [];
+  const [notes, showNotes] = useState([
+    {
+      title: "note 1",
+      body: "description for note 1",
+      priority: "high",
+      label: "work",
+    },
+    {
+      title: "note 2",
+      body: "description for note 2",
+      priority: "medium",
+      label: "home",
+    },
+    {
+      title: "note 3",
+      body: "description for note 3",
+      priority: "low",
+      label: "other",
+    },
+    {
+      title: "note 4",
+      body: "description for note 4",
+      priority: "low",
+      label: "other",
+    },
+  ]);
   return (
     <>
       <Header />
