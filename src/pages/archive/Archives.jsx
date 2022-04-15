24da0a9044dd1838b5149b88d8ed26a50170ab53
aishwarya-bm <biradaraishwarya@gmail.com>
2022-04-16 00:34:45 +0530

    created UI for archive, trash pages

--- src/pages/archive/Archives.jsx
+++ src/pages/archive/Archives.jsx
@@ -1,10 +1,42 @@
 import "./archives.css";
-import { Header, Sidenav } from "components";
+import { Header, NoteCard, Sidenav } from "components";
+import { useState } from "react";
 export function Archives() {
+  const notes = [];
   return (
     <>
       <Header />
       <Sidenav />
+      <div className="notes-container">
+        <h3 className="text-center">Archives - {notes.length}</h3>
+        {notes.length === 0 ? (
+          <>
+            <div className="not-found">
+              <h5 className="text-center">
+                Your have no items in the archives!
+              </h5>
+              <div className="d-flex children-center img-not-found">
+                <img
+                  src="https://cdn.iconscout.com/icon/premium/png-64-thumb/archive-5124885-4277584.png"
+                  alt="archive"
+                />
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="d-flex notes-list">
+              {notes?.map((item, idx) => {
+                return (
+                  <>
+                    <NoteCard item={item} />
+                  </>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
--- src/pages/trash/Trash.jsx
+++ src/pages/trash/Trash.jsx
@@ -1,10 +1,40 @@
 import "./trash.css";
-import { Header, Sidenav } from "components";
+import { Header, NoteCard, Sidenav } from "components";
+import { useState } from "react";
 export function Trash() {
+  const notes = [];
   return (
     <>
       <Header />
       <Sidenav />
+      <div className="notes-container">
+        <h3 className="text-center">Trash - {notes.length}</h3>
+        {notes.length === 0 ? (
+          <>
+            <div className="not-found">
+              <h5 className="text-center">Your have no items in trash!</h5>
+              <div className="d-flex children-center img-not-found">
+                <img
+                  src="https://cdn.iconscout.com/icon/premium/png-64-thumb/trash-delete-replace-block-disabled-1-51529.png"
+                  alt="trash"
+                />
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="d-flex notes-list">
+              {notes?.map((item, idx) => {
+                return (
+                  <>
+                    <NoteCard item={item} />
+                  </>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
